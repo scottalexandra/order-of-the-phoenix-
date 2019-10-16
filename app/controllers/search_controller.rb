49b360798a90329fc92c0e39784d638dc313d579
Alex Robinson <alexandra.scott335@gmail.com>
2019-10-16 09:50:58 -0600

    pass in house dynmically, parse the json response, create member poro

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -1,16 +1,18 @@
 class SearchController < ApplicationController
   def index
     # make an api call to the characters endpoint
-    # iterate over the data and grab the necessary info
-    # format that data as a PORO
+    # iterate over the data and grab the necessary info format as PORO
     # display that information in the view
-
+    house = params[:house]
     response = Faraday.get("https://www.potterapi.com/v1/characters") do |req|
       req.params["key"]= ENV['harry_potter_api_key']
-      req.params["house"] = "Gryffindor"
+      req.params["house"] = house
       req.params["orderOfThePhoenix"]= true
     end
 
-
+    members_data = JSON.parse(response.body, symbolize_names: true)
+    members_data.each do |member_hash|
+      Member.new(member_hash)
+    end
   end
 end
